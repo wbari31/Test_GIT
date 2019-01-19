39af8d97e6bef53093f6e7dc1f5c5a59205ba255
Wasim Bari <wbari31@gmail.com>
2019-01-20 03:06:37 +0530

    commiting

--- src/com/was/Main2.java
+++ src/com/was/Main2.java
@@ -4,6 +4,7 @@ public class Main2 {
 	public static void main(String ...s){
 		System.out.println("Second main class F");
 		System.out.println("Line2");
+		System.out.println("Modification1");
 	}
 
 }
